d4606d291db3239e5b0c0dfdf654644d09e9e0ac
jonelrelucio <hojo@DESKTOP-OQUQTMO>
2023-11-04 23:33:11 +0100

    First Commit

---
+++ src/main/java/com/hojo/cravel/CravelApplication.java
@@ -0,0 +1,14 @@
+package com.hojo.cravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.Mapping;
+
+@SpringBootApplication
+public class CravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hojo/cravel/CravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hojo.cravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
